c9a8e205526c7a33ca549a277b7219fa1c3d13b5
diomidispt <diomidispt@gmail.com>
2026-09-10 21:56:29 +0300

    feat: adding new s3 bucket for pfizer

---
+++ envs/sauron-s3-bucket/DioProjects-us-east-1-sauron-s3-bucket-pfizer-DEV/state.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "sauron-cicd-tfstate"
+    key            = "sauron-s3-bucket/DioProjects-us-east-1-sauron-s3-bucket-pfizer-DEV/terraform.tfstate"
+    dynamodb_table = "sauron-cicd-tfstate"
+    encrypt        = "true"
+  }
+}
